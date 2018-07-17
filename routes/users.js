f186f8b698d3c245327f8d7420fd20cc1155eecd
qqliang <qqliang@cmp.com>
2018-07-17 19:38:10 +0800

    Add router in user

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.post("/test", function(req, res) {
+	var record = req.body;
+	console.log(req.body);
+	console.log("sn: " + req.body.sn + " ip: " + req.body.ip);
+	res.end(JSON.stringify({"sn":record.sn, "ip":record.ip}));
+});
+
 module.exports = router;
