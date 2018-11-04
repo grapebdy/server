2bc0c498ce1add70f31f7310779345fb71094462
qqliang <qqliang@cmp.com>
2018-11-04 14:18:00 +0800

    Add date

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,9 @@ router.get('/', function(req, res) {
 
 router.post("/test", function(req, res) {
 	var record = req.body;
-	console.log(req.body);
+//	console.log(req.body);
 	console.log("sn: " + req.body.sn + " ip: " + req.body.ip);
-	res.end(JSON.stringify({"sn":record.sn, "ip":record.ip}));
+	res.end(JSON.stringify({"sn":record.sn, "ip":record.ip, "cmd":"date"}));
 });
 
 module.exports = router;
